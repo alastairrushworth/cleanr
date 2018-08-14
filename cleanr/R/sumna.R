ed2de7bcf62adeac83ac6f396d107de921a01337
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-08-14 12:00:12 +0100

    fixed bug in type detection for sumna function

--- cleanr/R/sumna.R
+++ cleanr/R/sumna.R
@@ -1,7 +1,7 @@
 sumna_slow <- function(v) sum(is.na(v))
 
 sumna <- function(v){
-  if(class(v) %in% c("numeric", "character", "integer", "logical", "factor")){
+  if(any(class(v) %in% c("numeric", "character", "integer", "logical", "factor"))){
     if(class(v) == "numeric")   nasum <- na_numeric(v)
     if(class(v) == "character") nasum <- na_character(v)
     if(class(v) == "integer")   nasum <- na_integer(v)
