89415b9df3e033777c9bccf07ab78b378fade543
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-08-15 12:42:04 +0100

    Set default exclude nmeric columns in imbalance check to speed up

--- cleanr/R/report_imbalance.R
+++ cleanr/R/report_imbalance.R
@@ -1,24 +1,30 @@
-report_imbalance <- function(df, top_n = 10){
+report_imbalance <- function(df, top_n = 10, include_numeric = F){
   
   # perform basic column check on dataframe input
   check_df_cols(df)
   
-  # exclude columns containing lists
-  not_list <- function(v) !is.list(v)  
-  gdf      <- df %>% select_if(not_list)
-  cnames   <- colnames(gdf)
-  # function to find the percentage of the most common value in a vector
-  imb_cols       <- do.call("rbind", lapply(gdf, fast_table))
-  imb_cols$names <- colnames(gdf)
-  
   # print console title text
-  console_title("Top most imbalanced features")
+  console_title("Top most imbalanced features (exlc. numeric")
   
-  # get top ten most imbalance by common class and pass to histogrammer
-  imb_cols %>% 
-    arrange(desc(prop)) %>% 
-    slice(1:top_n) %>% 
-    dot_bars_imbalance
+  # exclude columns containing lists (or numeric if not specified)
+  col_exclude <- ifelse(include_numeric, 
+                        function(v) !is.list(v), 
+                        function(v) !(is.list(v) | is.numeric(v))) 
+  gdf         <- df %>% select_if(col_exclude)
+  if(ncol(gdf) > 0){
+    cnames      <- colnames(gdf)
+    # function to find the percentage of the most common value in a vector
+    imb_cols       <- do.call("rbind", lapply(gdf, fast_table))
+    imb_cols$names <- colnames(gdf)
+    
+    # get top ten most imbalance by common class and pass to histogrammer
+    imb_cols %>% 
+      arrange(desc(prop)) %>% 
+      slice(1:top_n) %>% 
+      dot_bars_imbalance
+  } else {
+    cat(silver("    << Not applicable >>\n"))
+  }
   
   # invisibly return the df for further summaries
   invisible(df)
--- cleanr/R/sumna.R
+++ cleanr/R/sumna.R
@@ -2,11 +2,11 @@ sumna_slow <- function(v) sum(is.na(v))
 
 sumna <- function(v){
   if(any(class(v) %in% c("numeric", "character", "integer", "logical", "factor"))){
-    if(class(v) == "numeric")   nasum <- na_numeric(v)
-    if(class(v) == "character") nasum <- na_character(v)
-    if(class(v) == "integer")   nasum <- na_integer(v)
-    if(class(v) == "factor")    nasum <- na_integer(v)
-    if(class(v) == "logical")   nasum <- na_logical(v)
+    if(any(class(v) == "numeric"))   nasum <- na_numeric(v)
+    if(any(class(v) == "character")) nasum <- na_character(v)
+    if(any(class(v) == "integer"))   nasum <- na_integer(v)
+    if(any(class(v) == "factor"))    nasum <- na_integer(v)
+    if(any(class(v) == "logical"))   nasum <- na_logical(v)
   } else {
     nasum <- sumna_slow(v)
   }
