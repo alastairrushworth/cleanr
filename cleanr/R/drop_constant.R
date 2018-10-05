36b790b2634c940b42450756bb5db6ce9559bb88
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-10-05 16:08:31 +0100

    Added drop constant function

---
+++ cleanr/R/drop_constant.R
@@ -0,0 +1,40 @@
+drop_constant <- function(df, verbose = T){
+  
+  # perform basic column check on dataframe input
+  check_df_cols(df)
+  unique_1 <- function(v) length(unique(v)) == 1
+  
+  # cols for removal
+  drop_cols <- c()
+  
+  # integer constant columns to remove
+  df_integer <- df %>% select_if(is.integer)
+  if(ncol(df_integer) > 0) drop_cols <- c(drop_cols, which(sapply(df_integer, sd) == 0))
+  
+  # numeric constant colums to remove
+  df_numeric <- df %>% select_if(is.numeric)
+  if(ncol(df_numeric) > 0) drop_cols <- c(drop_cols, which(sapply(df_numeric, sd) == 0))
+  
+  # character constant columns to remove
+  df_character <- df %>% select_if(is.character)
+  if(ncol(df_character) > 0) drop_cols <- c(drop_cols, which(sapply(df_character, unique_1)))
+  
+  # factor constant columns to remove
+  df_factor <- df %>% select_if(is.factor)
+  if(ncol(df_factor) > 0) drop_cols <- c(drop_cols, which(sapply(df_factor, unique_1)))
+  
+  # return a message, if requested
+  if(verbose){
+    if(length(drop_cols) > 1){
+      names_to_drop <- sort(unique(names(df)[drop_cols]))
+      cat("Constant columns dropped:\n ")
+      cat(paste("\U2022 ", names_to_drop, " \n", sep = ""))
+      df <- df %>% select(-drop_cols)
+    } else {
+      cat("No constant columns found.\n ")
+    }
+  }
+  
+  # invisibly return the df for further summaries
+  invisible(df)
+}
